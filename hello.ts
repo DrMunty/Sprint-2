fee2784fb01aab740697d8f2201bf2e58f1cbab9
albert-dev <al96@hotmail.es>
2026-05-03 16:38:04 +0200

    "tested union types and narrowing in TS"

--- hello.ts
+++ hello.ts
@@ -6,4 +6,11 @@ function greet(person: string, date: Date) {
 
 greet("Albert", new Date());
 
+function printId(id: number | string) {
+  console.log("Your ID is: " + id);
+}
+printId(101)
+printId("202")
+// printId({myID:2004}) Això no seria correcte ja que no s'ha especificat cap type.
+
 export {};
